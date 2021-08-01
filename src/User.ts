9173b64075603c31d7eebd4341bafe94a0b4324a
elliot.yibaebi <e.yibaebi@genesystechhub.com>
2021-08-01 11:13:44 +0100

    update implementation of custom type mappable of user and company classes;
    add markerContent method to create content meant for info window.

--- src/User.ts
+++ src/User.ts
@@ -1,6 +1,7 @@
 import faker from "faker";
+import { Mappable } from "./CustomMap";
 
-export class User {
+export class User implements Mappable {
   name: string;
   location: {
     lat: number;
@@ -14,4 +15,12 @@ export class User {
         lng: parseFloat(faker.address.longitude()),
       });
   }
+
+  markerContent = (): string => {
+    return `
+        <div>
+            <h4>Username: ${this.name}</h4>
+        </div>
+      `;
+  };
 }
